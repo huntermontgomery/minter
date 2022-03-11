e935fa01da3b57c109b5c43ad8afeb7d107e7221
huntermontgomery <hunter_montgomery@outlook.com>
2022-03-11 22:47:37 +1100

    initial commit

--- src/minter_assets/src/index.js
+++ src/minter_assets/src/index.js
@@ -1,19 +1,30 @@
+// The minter is the representation of the minter contract in main.mo but in JavaScript
 import { minter } from "../../declarations/minter";
 
-document.querySelector("form").addEventListener("submit", async (e) => {
-  e.preventDefault();
-  const button = e.target.querySelector("button");
+// This is library to use with principal that is provided by Dfinity
+import { Principal } from "@dfinity/principal";
 
+// For beginners : This is really basic Javascript code that add an event to the "Mint" button so that the mint_nft function is called when the button is clicked.
+const mint_button = document.getElementById("mint");
+mint_button.addEventListener("click", mint_nft);
+
+async function mint_nft() {
+  // Get the url of the image from the input field
   const name = document.getElementById("name").value.toString();
+  console.log("The url we are trying to mint is " + name);
 
-  button.setAttribute("disabled", true);
+  // Get the principal from the input field.
+  const principal_string = document.getElementById("principal").value.toString();
+  const principal = Principal.fromText(principal_string);
 
-  // Interact with foo actor, calling the greet method
-  const greeting = await minter.greet(name);
+  // Mint the image by calling the mint_principal function of the minter.
+  const mintId = await minter.mint_principal(name, principal);
+  console.log("The id is " + Number(mintId));
+  // Get the id of the minted image.
 
-  button.removeAttribute("disabled");
+  // Get the url by asking the minter contract.
+  document.getElementById("nft").src = await minter.tokenURI(mintId);
 
-  document.getElementById("greeting").innerText = greeting;
-
-  return false;
-});
+  // Show some information about the minted image.
+  document.getElementById("greeting").innerText = "this nft owner is " + principal_string + "\nthis token id is " + Number(mintId);
+}
